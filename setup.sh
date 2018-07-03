44edd8d2ff4ad05d04ba8606e60de9cf90390c56
John Naylor <jonaylor89@gmail.com>
2018-07-03 15:33:25 -0400

    New setup file

---
+++ setup.sh
@@ -0,0 +1,11 @@
+
+# A work in progress
+
+sudo pacman -S git zsh tmux vim 
+
+git clone https://github.com/jonaylor89/dotfiles.git
+
+ln ~/dotfiles/zshrc .zshrc
+ln ~/dotfiles/vimrc .vimrc
+ln ~/dotfiles/tmux.conf .tmux.conf
+
