cc8e53d3f742380035ae24575bcbe01ac61e9594
John Naylor <jonaylor89@gmail.com>
2018-08-09 17:41:45 -0400

    Adding plugins

--- setup.sh
+++ setup.sh
@@ -9,6 +9,9 @@ git clone https://github.com/jonaylor89/dotfiles.git ~/dotfiles
 # Download oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# Download autosuggestions 
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 # Download powerlevel9k zsh theme
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/
 
