8418ea4d3745340c9526a3559a66baff8ebda6f5
John Naylor <jonaylor89@gmail.com>
2018-11-29 17:00:09 -0500

    Python version

---
+++ update_git_repos.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import subprocess
+from multiprocessing import Pool
+
+def update_repo(repo):
+    print("\n\x1b[95m=-=-={" + repo + "}=-=-=\x1b[m\n")
+    subprocess.run(['git', '--git-dir='+os.path.join(repo, '.git'),
+                    '--work-tree='+os.path.join(repo, '..'),
+                    'pull', 'origin', 'master']) 
+    
+
+if __name__ == '__main__':
+
+    print("\n\x1b[32mPulling in later changes for all repositories...\x1b[m\n")
+
+    default_path = os.environ['HOME'] + '/Repos'
+
+    if len(sys.argv) < 2:
+        repos = os.listdir(default_path)
+        repos = [os.path.join(default_path, repo) for repo in repos]
+    else:
+        repos = os.listdir(os.getcwd())
+        repos = [os.path.join(os.getcwd(), repo) for repo in repos]
+
+    with Pool(5) as p:
+        p.map(update_repo, repos)
+    
+    print("\n\x1b[32mComplete!\x1b[m\n")
