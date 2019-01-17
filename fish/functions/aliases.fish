382f6b4b61ceadc3dd898c752977effcf4f45f4e
John Naylor <jonaylor89@gmail.com>
2019-01-17 13:23:26 -0500

    Add aliases

--- fish/functions/aliases.fish
+++ fish/functions/aliases.fish
@@ -1,5 +1,18 @@
 #!/usr/bin/env fish
 
+alias la="exa -abghl --git"
+alias ll="exa -bghl --git"
+alias lt="exa --tree"
+alias cls="clear"
+alias rmd="rm -rf"
+alias r='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
+alias fetch="neofetch"
+alias al="sl -alf"
+
+alias gls="gitlocalstats"
+alias ugr="update_git_repos"
+alias rainbow="lolcat"
+
 function serve
     if test (count $argv) -ge 1
         if python -c 'import sys; sys.exit(sys.version_info[0] != 3)'
