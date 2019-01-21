29b8184782969bd10590fceeaacf2e1c29b062ba
John Naylor <jonaylor89@gmail.com>
2019-01-21 11:01:32 -0500

    splitting things up

---
+++ zshrc.d/aliases.zsh
@@ -0,0 +1,20 @@
+
+ # Configs
+ alias zshrc="vim ~/.zshrc"
+ alias tmuxconf="vim ~/.tmux.conf"
+ alias vimrc="vim ~/.vimrc"
+
+ # abbr
+ alias la="exa -abghl --git"
+ alias ll="exa -bghl --git"
+ alias lt="exa --tree"
+ alias cls="clear"
+ alias rmd="rm -rf"
+ alias r="ranger"
+ alias fetch="neofetch"
+
+ # Custom bins
+ alias gls="gitlocalstats"
+ alias ugr="update_git_repos"
+
+
---
+++ zshrc.d/exports.zsh
@@ -0,0 +1,14 @@
+
+# All path locations
+export PATH=$HOME/bin:/usr/local/bin:$HOME/.local/bin:$PATH
+
+# Path to your oh-my-zsh installation.  
+export ZSH=$HOME/.oh-my-zsh
+
+# Terminal Things
+export LANG="en_US.UTF-8"
+export TERM=xterm-256color
+export EDITOR=vim
+
+# ssh
+export SSH_KEY_PATH="~/.ssh/rsa_id"
