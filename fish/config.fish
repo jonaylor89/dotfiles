991a33f6c031c325226ab8ef49e894f6c9ec94e3
John Naylor <jonaylor89@gmail.com>
2019-02-14 19:26:47 -0500

    Submodule

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,2 @@
 
-set PATH /usr/local/bin /usr/sbin $HOME/bin $HOME/Library/Python/3.7/bin $PATH
+set PATH /usr/local/bin /usr/sbin $HOME/bin $HOME/Library/Python/3.7/bin $HOME/Library/Application\ Support/Übersicht/widgets/Xanthia $PATH
