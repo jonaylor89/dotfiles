ea7badb73fc7babcde1414adc0dc85b6284d9318
John Naylor <jonaylor89@gmail.com>
2019-02-15 10:24:32 -0500

    Pecan style configuring

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,4 @@
 
-set PATH /usr/local/bin /usr/sbin $HOME/bin $HOME/Library/Python/3.7/bin $HOME/Library/Application\ Support/Übersicht/widgets/Xanthia $PATH
+set PATH /usr/local/bin /usr/sbin $HOME/bin $HOME/Library/Python/3.7/bin $HOME/dotfiles/widgets/Pecan/ $PATH
+
+export UBER_PATH="$HOME/Repos/dotfiles/" 
