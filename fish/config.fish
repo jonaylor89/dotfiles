103ebc691cf19eaa754b7e1331dc3dad683da268
John Naylor <jonaylor89@gmail.com>
2019-03-03 00:40:05 -0500

    Update chunkwm

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,5 @@
 set PATH /usr/local/bin /usr/sbin $HOME/bin $HOME/Library/Python/3.7/bin $HOME/dotfiles/widgets/Pecan/ $PATH
 
 export UBER_PATH="$HOME/Repos/dotfiles/" 
+
+wal -i ~/Pictures/take-me-to-the-moon-2560x1440.jpg -n
