a8b8ca1f624639c1cf2ec14a0753676121b24450
John Naylor <jonaylor89@gmail.com>
2019-04-15 15:33:54 -0400

    Window Manager

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,13 @@ set PATH /usr/local/bin /usr/sbin $HOME/bin $HOME/Library/Python/3.7/bin $HOME/d
 export UBER_PATH="$HOME/Repos/dotfiles/" 
 
 wal -q -i ~/Pictures/DigitSun.png -n
+
+# tabtab source for serverless package
+# uninstall by removing these lines or running `tabtab uninstall serverless`
+[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish
+# tabtab source for sls package
+# uninstall by removing these lines or running `tabtab uninstall sls`
+[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish
+# tabtab source for slss package
+# uninstall by removing these lines or running `tabtab uninstall slss`
+[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.fish
